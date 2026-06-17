4f1260f7594a6c41035acf202593b6bcafe4ced5
Mahlia Stallworth-Lewis <mahlialws@Mahlias-MacBook-Pro.local>
2026-06-17 16:33:05 -0700

    Add Python profile variables lab

---
+++ profile.py
@@ -0,0 +1,14 @@
+
+full_name = "Mahlia Stallworth-Lewis"
+
+nickname = "May"
+
+age = 17
+
+used_python_before = False
+
+hobbies = ["reading", "crocheting", "sleeping"]
+
+favorites = {"movie": "Dirty Dancing", "food": "pesto pasta", "hobby": "reading"}
+
+print(full_name, nickname, age, used_python_before, hobbies, favorites)
